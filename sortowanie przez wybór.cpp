4ea25ce97b14a781cd2402d337287a0c121ac237
league489 <107813326+league489@users.noreply.github.com>
2023-03-18 17:48:43 +0100

    Add files via upload

---
+++ sortowanie przez wybór.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int n = 6;
+    int a[n] = {-2,0,4,-3,2,7};
+
+    for(int i=0;i<n-1;i++){
+    int min = i;
+for( int j = i+1;j<n;j++){
+    if(a[j]<a[min])
+    {
+        min = j;
+        cout<< i <<" "<< min <<" " << j <<" ( "<<a[j]<<" < "<<a[min]<<" ) "<< "T" <<endl;
+    }
+    else{
+         cout<< i <<" "<< min <<" " << j <<"( "<<a[j]<<" < "<<a[min] <<" ) "<< "F" <<endl;
+    }
+}
+int t = a[min];
+a[min] = a[i];
+a[i] = t;
+cout<<"t = " << t <<endl;
+for (int c = 0;c<n;c++)
+{
+    cout<<a[c]<<" , ";
+}
+cout<<endl;
+}
+}
+
