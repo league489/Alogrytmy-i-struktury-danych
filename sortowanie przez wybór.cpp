6c3b80187a450b8fb738b53be4f7888bc7f31012
league489 <107813326+league489@users.noreply.github.com>
2023-03-18 17:49:51 +0100

    Update sortowanie przez wybór.cpp

--- sortowanie przez wybór.cpp
+++ sortowanie przez wybór.cpp
@@ -1,17 +1,10 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
 
 int main()
-{int n = 6;
+{
+  int n = 6;
     int a[n] = {-2,0,4,-3,2,7};
 
     for(int i=0;i<n-1;i++){
